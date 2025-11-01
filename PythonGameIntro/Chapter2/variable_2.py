24e9eb7948a66233fae2c07ae384dc26964fc713
fuj1tuki3 <fuj1tuki3@gmail.com>
2025-11-01 14:22:40 +0900

    Delete PythonGameIntro directory

--- PythonGameIntro/Chapter2/variable_2.py
+++
@@ -1,4 +0,0 @@
-score = 0
-print("スコアの初期値は", score)
-score = score + 100
-print("計算後のスコアは", score)
--- PythonGameIntro/Chapter5/rand_seed.py
+++
@@ -1,5 +0,0 @@
-import random
-random.seed(0)
-for i in range(20):
-    r = random.randint(0, 99)
-    print(r, end=",")
